5da6f7dd334fcfc1e9f1fb8c993ba1c5120fc680
zhaksylyk1 <zhaksylyk.kuanysh@nu.edu.kz>
2024-03-26 17:10:46 +0600

    asd

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from .models import Video
 
 class VideoForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Title', 'class': 'form-control'}
+        ),
+        label=''  # This ensures that the label is not displayed
+    )
+    video = forms.FileField(
+        widget=forms.FileInput(
+            attrs={'id': 'video', 'hidden': True}
+        ),
+        label=''  # This ensures that the label is not displayed
+    )
+
     class Meta:
         model = Video
         fields = ['title', 'video']
